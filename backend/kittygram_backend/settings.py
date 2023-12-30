1ca5011081188270620545f502b3675766bfe0f3
Alexander <kudriavtcevs@bk.ru>
2023-12-30 23:04:11 +0300

    Kittygram_8

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django')
 
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.1, localhost, fedor-aaappp.ddns.net').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1, localhost, fedor-aaappp.ddns.net').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
