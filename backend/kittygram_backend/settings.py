927fd8d8961a718b413d86f1a0092792e97d1c2f
Alexander <kudriavtcevs@bk.ru>
2024-01-11 10:47:56 +0300

    Kittygram_11

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -5,10 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'django')
 
-if os.getenv('DEBUG') == 'True':
-    DEBUG = True
-else:
-    DEBUG = False
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1, localhost, fedor-aaappp.ddns.net').split(',')
 
